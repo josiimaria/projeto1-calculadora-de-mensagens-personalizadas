bbe475fd569ad239d38f99e892ce3b1721fe16bb
maria josiane <maria.moreira489@aluno.ce.gov.br>
2025-08-13 16:41:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let nome = "sofia"
+let idade = 20
+let cidade = "canide"
+let profissao = "veterinaria"
+let anoAtual = 2025 
+let anoNacimento = Number(anoAtual) - Number(idade) 
+
+let salario = 2000
+let bonus = 2
+let total = Number (salario) * Number(bonus)
+  
+alert (` 
+ nome: ${nome}
+ idade : ${idade}
+ cidade:${cidade}
+ profissao: ${profissao}
+ anoAtual : ${anoAtual}
+ anoNacimento : ${anoNacimento}
+  `)
