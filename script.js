ad898b1bc556d1a022e379497ee528f9a5b29072
maria josiane <maria.moreira489@aluno.ce.gov.br>
2025-08-19 15:47:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ let idade = 20
 let cidade = "canide"
 let profissao = "veterinaria"
 let anoAtual = 2025 
-let anoNacimento = Number(anoAtual) - Number(idade) 
+let anoNacimento = parseInt(anoAtual) -parseInt(idade) 
 
 let salario = 2000
 let bonus = 2
-let total = Number (salario) * Number(bonus)
+let total = parseInt(salario) * parseInt(bonus)
   
 alert (` 
- nome: ${nome}
- idade : ${idade}
- cidade:${cidade}
- profissao: ${profissao}
- anoAtual : ${anoAtual}
- anoNacimento : ${anoNacimento}
-  `)
+nome: ${nome}
+idade : ${idade}
+cidade:${cidade}
+profissao: ${profissao}
+anoAtual : ${anoAtual}
+anoNacimento : ${anoNacimento}
+salario: ${salario} 
+bonus: ${bonus}
+total: ${total}`)
